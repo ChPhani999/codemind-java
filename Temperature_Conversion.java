d3c55a6096b57a0c3b3f9c8c6589bff9fa97a784
SURYA PHANEENDRA CHAGANTI <97582344+ChPhani999@users.noreply.github.com>
2023-02-01 11:56:48 +0530

    import java.util.*; class temp {     public static void main(String args[])     {     Scanner sc=new Scanner(System.in);     float c=sc.nextFloat();     double f=32+(c*9/5);     System.out.format("%.2f",f);
        }
    }

---
+++ Temperature_Conversion.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class temp
+{
+    public static void main(String args[])
+    {
+    Scanner sc=new Scanner(System.in);
+    float c=sc.nextFloat();
+    double f=32+(c*9/5);
+    System.out.format("%.2f",f);
+    
+    
+        
+    }
+}
