6ece5aa6f1668bd4afd81e3ab280ae6740aa564d
SURYA PHANEENDRA CHAGANTI <97582344+ChPhani999@users.noreply.github.com>
2023-02-01 14:20:03 +0530

    import java.util.*; class div {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int p=sc.nextInt();         int q=sc.nextInt();         System.out.println(p%q);     }
    }

---
+++ Modulus.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class div
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int p=sc.nextInt();
+        int q=sc.nextInt();
+        System.out.println(p%q);
+    }
+    
+}
