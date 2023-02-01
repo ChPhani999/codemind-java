095a492363b7e45af34358609edfbc30bbc859bd
SURYA PHANEENDRA CHAGANTI <97582344+ChPhani999@users.noreply.github.com>
2023-02-01 14:13:31 +0530

    import java.util.*; class div {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         float n= sc.nextFloat();         float m=sc.nextFloat();         float p=n*m;         System.out.format("%.2f",p);     }
    }

---
+++ Program_to_Multiply_Two_Floating-Point_Numbers.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class div
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        float n= sc.nextFloat();
+        float m=sc.nextFloat();
+        float p=n*m;
+        System.out.format("%.2f",p);
+    }
+    
+}
